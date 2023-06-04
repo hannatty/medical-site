c597e1b1506b8d2745a116d292cd5a0ab39c67ec
Rafael Barros <leafar.barros@gmail.com>
2023-06-04 13:55:42 -0300

    correção footer web desktop

--- src/components/FooterPage/index.tsx
+++ src/components/FooterPage/index.tsx
@@ -7,22 +7,19 @@ import facebook_icon from '../../../public/images/footer_icons/facebook_footer_i
 
 export default function FooterPage(){
     return(
-        <div className={styles.footer_container}>
-            {/* <div className={styles.footer_professional}>
-                <div className={styles.footer_content}>
+        <div className={styles.main_footer_container}>
+            <div className={styles.internal_footer_container }>
+                <div className={styles.footer_professional_information}>
+                    <h3>Dr. Carlos Alberto Barros</h3>
+                    <span>Cirurgião de Cabeça e Pescoço</span>
+                    <span>CRM-PE: 18069 | RQE: 3263</span>
                 </div>
-            </div> */}
-            <div className={styles.footer_professional_information}>
-                <h3>Dr. Carlos Alberto Barros</h3>
-                <span>Cirurgião de Cabeça e Pescoço</span>
-                <span>CRM-PE: 18069 | RQE: 3263</span>
+                <div className={styles.footer_social_media_links}>
+                    <Link href="https://api.whatsapp.com/send/?phone=5581993524086&text=Ol%C3%A1%21+Gostaria+de+agendar+uma+consulta.&type=phone_number&app_absent=0"><Image src={whatsapp_icon} alt=''className={styles.footer_icon}></Image></Link>
+                    <Link href="https://www.instagram.com/drcarlos.cabecaepescoco/"><Image src={instagram_icon} alt='' className={styles.footer_icon}></Image></Link>
+                    <Link href="https://www.facebook.com/profile.php?id=100054473439582"><Image src={facebook_icon} alt='' className={styles.footer_icon}></Image></Link>
+                </div>
             </div>
-            <div className={styles.footer_social_media_links}>
-                <Link href="https://api.whatsapp.com/send/?phone=5581993524086&text=Ol%C3%A1%21+Gostaria+de+agendar+uma+consulta.&type=phone_number&app_absent=0"><Image src={whatsapp_icon} alt=''className={styles.footer_icon}></Image></Link>
-                <Link href="https://www.instagram.com/drcarlos.cabecaepescoco/"><Image src={instagram_icon} alt='' className={styles.footer_icon}></Image></Link>
-                <Link href="https://www.facebook.com/profile.php?id=100054473439582"><Image src={facebook_icon} alt='' className={styles.footer_icon}></Image></Link>
-            </div>
-
         </div>
     )
 }
